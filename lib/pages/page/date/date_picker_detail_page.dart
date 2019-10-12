d7aefe3b48b121a793f9ab9b772e7076fcd50f4b
wujinli <wujinli>
2019-10-12 15:16:35 +0800

    集成三方时间选择组件，

--- lib/pages/page/date/date_picker_detail_page.dart
+++ lib/pages/page/date/date_picker_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class DatePickerDetailPage extends StatefulWidget {
   final arguments;
@@ -17,6 +18,10 @@ class _DatePickerDetailState extends State<DatePickerDetailPage> {
   var _nowDate = DateTime.now(); //初始化的日期
   var _nowTime = TimeOfDay(
       hour: DateTime.now().hour, minute: DateTime.now().minute); //初始化的时间
+
+  final String MIN_DATETIME = "1980-05-12 05:15:20";
+  final String MAX_DATETIME = "2100-05-12 05:15:20";
+  final String dateformat = 'YYYY-mm-dd';
 
   _DatePickerDetailState({this.arguments});
 
@@ -70,7 +75,8 @@ class _DatePickerDetailState extends State<DatePickerDetailPage> {
                   ),
                   onTap: () {
 //                    this._showDatePickerNormal();
-                    this._showDatePickerAsync();
+//                    this._showDatePickerAsync();
+                    this._checkDate();
                   },
                 ),
                 InkWell(
@@ -81,8 +87,9 @@ class _DatePickerDetailState extends State<DatePickerDetailPage> {
                     ],
                   ),
                   onTap: () {
-//                    this._showTimePickerNormal();
-                    this._showTimePickerAsync();
+////                    this._showTimePickerNormal();
+//                    this._showTimePickerAsync();
+                    this._checkTime();
                   },
                 ),
               ],
@@ -93,6 +100,36 @@ class _DatePickerDetailState extends State<DatePickerDetailPage> {
     );
   }
 
+  void _checkDate() {
+    switch (typeFlag) {
+      case 1:
+//      this._showDatePickerNormal();
+        this._showDatePickerAsync();
+        break;
+      case 2:
+        _checkDateByDatePicker();
+        break;
+    }
+  }
+
+  void _checkTime() {
+    switch (typeFlag) {
+      case 1:
+        // this._showTimePickerNormal();
+        this._showTimePickerAsync();
+        break;
+      case 2:
+        this._checkTimeByTimePicker();
+        break;
+    }
+  }
+
+
+  /**
+   * 注意：DateTime与TimeOfDay之间的用法
+   */
+
+  /******************************系统日期选择器******************************/
   /**
    * 获取日期的方法
    */
@@ -144,5 +181,54 @@ class _DatePickerDetailState extends State<DatePickerDetailPage> {
     setState(() {
       if (result != null) this._nowTime = result;
     });
+  }
+
+  /*******************************三方依赖库实现日期时间选择************************************/
+
+  /**
+   * 当前使用的是三方依赖库实现时间日期的选择
+   * 1.配置三方依赖库：dependencies: flutter_datetime_picker: ^1.2.6
+   * 2.使用时引入包：import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+   * 三方依赖包github地址：https://github.com/Realank/flutter_datetime_picker
+   */
+
+  /**
+   * 展示日期选择组件
+   * 日期组件 DatePicker.showDatePicker（）
+   * minTime：起始时间
+   * maxTime：截止最大时间
+   * onConfirm:确定回调
+   * currentTime：当前日期
+   * locale:显示语言
+   */
+  void _checkDateByDatePicker() {
+    DatePicker.showDatePicker(context,
+        showTitleActions: true,
+        minTime: DateTime(2018, 3, 5),
+        maxTime: DateTime(2100, 6, 7), onConfirm: (date) {
+      setState(() {
+        this._nowDate = date;
+      });
+    }, currentTime: DateTime.now(), locale: LocaleType.zh);
+  }
+
+  /**
+   * 展示时间选择组件
+   * 时间组件： DatePicker.showTimePicker
+   * currentTime：当前时间
+   *
+   */
+  void _checkTimeByTimePicker() {
+    DatePicker.showTimePicker(context, showTitleActions: true,
+        onConfirm: (date) {
+//      print(date);
+//      print(date.hour);
+//      print(date.minute);
+      setState(() {
+        this._nowTime = TimeOfDay(hour: date.hour, minute: date.minute);
+//        print(this._nowTime.format(context));
+//        print(this._nowTime);
+      });
+    }, currentTime: DateTime.now(), locale: LocaleType.zh);
   }
 }
