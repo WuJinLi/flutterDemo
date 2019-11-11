ab9a5ff64704e4116fda1484bd6c7c432c913542
wujinli <269316671@qq.COM>
2019-11-11 21:09:15 +0800

    修复输入框demo页面输入框导致组件溢出屏幕的bug

--- lib/pages/page/form/text_field.dart
+++ lib/pages/page/form/text_field.dart
@@ -49,147 +49,149 @@ class _TextFiledState extends State<TextFiledPage> {
       ),
       body: Container(
           child: Padding(
-        padding: EdgeInsets.all(5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 5),
-              child: Text(
-                "默认显示:",
-                style: TextStyle(fontSize: 15, color: Colors.green),
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            TextField(
-              controller: _username,
-              onChanged: (value) {
-                setState(() {
-                  this._content = value;
-                });
-              },
-              autofocus: true,
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 5),
-              child: Text(
-                "包含提示文字:",
-                style: TextStyle(fontSize: 15, color: Colors.green),
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "输入内容",
-                border: OutlineInputBorder(), //设置输入框边框
-              ),
-              maxLines: 1,
-              onChanged: (value) {
-                setState(() {
-                  this._content = value;
-                });
-              },
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 5),
-              child: Text(
-                "密码输入框:",
-                style: TextStyle(fontSize: 15, color: Colors.green),
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "输入密码",
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true, //密码输入格式
-              maxLines: 1,
-              onChanged: (value) {
-                setState(() {
-                  this._content = value;
-                });
-              },
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 5),
-              child: Text(
-                "label:",
-                style: TextStyle(fontSize: 15, color: Colors.green),
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            TextField(
-              maxLines: 1,
-              decoration: InputDecoration(
-                  icon: Icon(Icons.people), //输入框前边设置图标
-                  labelText: "用户名",
-                  border: OutlineInputBorder()),
-              onChanged: (value) {
-                setState(() {
-                  this._content = value;
-                });
-              },
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            TextField(
-              maxLines: 1,
-              decoration: InputDecoration(
-                  icon: Icon(Icons.lock),
-                  labelText: "密码",
-                  border: OutlineInputBorder()),
-              obscureText: true,
-              onChanged: (value) {
-                setState(() {
-                  this._content = value;
-                });
-              },
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 5),
-              child: Text(
-                "输入框输入的内容:",
-                style: TextStyle(fontSize: 15, color: Colors.green),
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 40,
-              color: Colors.grey,
-              child: Text(
-                this._content,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
-      )),
+              padding: EdgeInsets.all(5),
+              child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Text(
+                          "默认显示:",
+                          style: TextStyle(fontSize: 15, color: Colors.green),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      TextField(
+                        controller: _username,
+                        onChanged: (value) {
+                          setState(() {
+                            this._content = value;
+                          });
+                        },
+                        autofocus: true,
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Text(
+                          "包含提示文字:",
+                          style: TextStyle(fontSize: 15, color: Colors.green),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "输入内容",
+                          border: OutlineInputBorder(), //设置输入框边框
+                        ),
+                        maxLines: 1,
+                        onChanged: (value) {
+                          setState(() {
+                            this._content = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Text(
+                          "密码输入框:",
+                          style: TextStyle(fontSize: 15, color: Colors.green),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: "输入密码",
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true, //密码输入格式
+                        maxLines: 1,
+                        onChanged: (value) {
+                          setState(() {
+                            this._content = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Text(
+                          "label:",
+                          style: TextStyle(fontSize: 15, color: Colors.green),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      TextField(
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                            icon: Icon(Icons.people), //输入框前边设置图标
+                            labelText: "用户名",
+                            border: OutlineInputBorder()),
+                        onChanged: (value) {
+                          setState(() {
+                            this._content = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      TextField(
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                            icon: Icon(Icons.lock),
+                            labelText: "密码",
+                            border: OutlineInputBorder()),
+                        obscureText: true,
+                        onChanged: (value) {
+                          setState(() {
+                            this._content = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Text(
+                          "输入框输入的内容:",
+                          style: TextStyle(fontSize: 15, color: Colors.green),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        width: double.infinity,
+                        height: 40,
+                        color: Colors.grey,
+                        child: Text(
+                          this._content,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+              )
+          )),
     );
   }
 }
