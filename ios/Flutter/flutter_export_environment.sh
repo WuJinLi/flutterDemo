3a7ef346c47e18cf6c48229e45a63de43bfdad95
wujinli <wujinli>
2019-11-29 10:21:06 +0800

    aa

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/WuJinLi/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/WuJinLi/AndroidStudioProjects/flutter_learn"
+export "FLUTTER_APPLICATION_PATH=/Users/WuJinLi/AndroidStudioProjects/flutterdemo"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
