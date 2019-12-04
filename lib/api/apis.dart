48fc5cd7f22b8d8bbca2f4f427e6307721b9fc0f
wujinli <wujinli>
2019-12-04 17:12:46 +0800

    优化和风请求

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -10,7 +10,7 @@ class Apis {
   static const String WEATHER_TYPE_LIFESTYLE = 'lifestyle'; //	生活指数
   static const String WEATHER_TYPE_HOURLY = 'hourly'; //	逐小时预报
 
-//  static const String BASE_HOST = "https://free-api.heweather.net"; //和风天气
+  static const String BASE_HOST_W = "https://free-api.heweather.net"; //和风天气
 
   static const String BASE_HOST = 'http://app.wisdomrui.com/'; //
 
@@ -20,31 +20,31 @@ class Apis {
       {location = 'beijing', weatherType = WEATHER_TYPE_NOW}) {
     switch (weatherType) {
       case WEATHER_TYPE.NOW:
-        return BASE_HOST +
+        return BASE_HOST_W +
             '/s6/weather/now?location=' +
             location +
             '&key=' +
             Constants.APP_KEY;
       case WEATHER_TYPE.FORECAST:
-        return BASE_HOST +
+        return BASE_HOST_W +
             '/s6/weather/forecast?location=' +
             location +
             '&key=' +
             Constants.APP_KEY;
       case WEATHER_TYPE.LIFESTYLE:
-        return BASE_HOST +
+        return BASE_HOST_W +
             '/s6/weather/lifestyle?location=' +
             location +
             '&key=' +
             Constants.APP_KEY;
       case WEATHER_TYPE.HOURLY:
-        return BASE_HOST +
+        return BASE_HOST_W +
             '/s6/weather/hourly?location=' +
             location +
             '&key=' +
             Constants.APP_KEY;
       default:
-        return BASE_HOST +
+        return BASE_HOST_W +
             '/s6/weather/now?location=' +
             location +
             '&key=' +
